a34ac20573a041f9829906abb9f689a86b506567
tonieichelkraut <toni@eichelkraut.org>
2020-11-15 23:11:38 +0100

    first set of game rules

--- Bowling.hs
+++ Bowling.hs
@@ -2,16 +2,39 @@ module Bowling where
 
 import Text.Read
 import Control.Monad
+import Data.List
 
 newtype Roll = Roll{value::Int} deriving(Eq)
 
-data Frame = Strike | Spare {firstRoll::Roll} | Normal {firstRoll::Roll,secondRoll::Roll} | Bonus{firstRoll::Roll,secondRoll::Roll,thridRoll::Roll}
+data CompleteFrame = Strike | Spare {firstRoll::Roll} | Normal {firstRoll::Roll,secondRoll::Roll} | Bonus{firstRoll::Roll,secondRoll::Roll,thridRoll::Roll} deriving(Eq)
+type CurrentFrame = [Roll]
+
+type Game = ([CompleteFrame],CurrentFrame)
 
 instance Show Roll where
     show = show.value
 
-int2Roll :: Int -> Roll
-int2Roll i = Roll{value=i}
+instance Show CompleteFrame where
+    show f = case f of
+              Strike ->             " X "
+              Spare{firstRoll=f} -> show f++" /"
+              Normal{firstRoll=f,secondRoll=s} -> show f++" "++show s
+
+showGame :: Game -> String
+showGame (fs,[]) = foldl (\a b -> a++"|"++b) "" (map show fs)
+showGame (fs,[c]) = foldl (\a b -> a++"|"++b) "" (map show fs) ++ "|" ++show c
+
+readRoll :: IO (Maybe Roll)
+readRoll = string2Roll <$> getLine
+
+roll :: Int -> Roll
+roll i = Roll{value=i}
+
+normal :: Int -> Int -> CompleteFrame
+normal f s = Normal{firstRoll=roll f,secondRoll=roll s}
+
+spare :: Int -> CompleteFrame
+spare f = Spare{firstRoll=roll f}
 
 isValidInt :: Int -> Bool
 isValidInt i = i>=0 && i<=10
@@ -20,6 +43,21 @@ string2Roll :: String -> Maybe Roll
 string2Roll r = do
     i <- readMaybe r
     guard(isValidInt i)
-    return (int2Roll i)
+    return (roll i)
 
-
+addRoll :: Game -> Roll -> Game
+addRoll g0@(fs,[]) r =  let score = value r
+                            update
+                               | score < 10  = (fs,[r])
+                               | score == 10 = (fs ++ [Strike],[])
+                               | otherwise   = g0
+                        in update
+addRoll g0@(fs,[f]) s = let combined = value f + value s
+                            includeRollAsNormal =(fs ++ [Normal{firstRoll=f,secondRoll=s}],[])
+                            includeRollAsSpare =(fs ++ [Spare{firstRoll=f}],[])
+                            ignoreRoll = g0
+                            update
+                               | combined < 10  = includeRollAsNormal
+                               | combined == 10 = includeRollAsSpare
+                               | otherwise      = ignoreRoll
+                        in update
--- testBowling.hs
+++ testBowling.hs
@@ -1,25 +1,50 @@
 import Test.HUnit
 import Bowling
+import Data.Tuple.Curry
+import Data.Maybe
 
 testDataString2Roll :: [([Char], Maybe Roll)]
 testDataString2Roll = [
-    ("0",Just Roll{value=0}),
-    ("5",Just Roll{value=5}),
-    ("10",Just Roll{value=10}),
+    ("0",Just (roll 0)),
+    ("5",Just (roll 5)),
+    ("10",Just (roll 10)),
     ("11",Nothing),
     ("-1",Nothing),
     ("x",Nothing)
     ]
-                    
+
 message :: String -> Maybe Roll -> String
 message s r = "computing " ++ show r ++ " from " ++ s ++ " failed."
 
-test_string2Roll :: String -> Maybe Roll -> Test
-test_string2Roll s r = TestCase (assertEqual (message s r) r (string2Roll s))
+testString2Roll :: String -> Maybe Roll -> Test
+testString2Roll s r = TestCase (assertEqual (message s r) r (string2Roll s))
 
-tests_string2Roll :: Test
-tests_string2Roll = TestList (map (uncurry test_string2Roll) testDataString2Roll)
+testsString2Roll :: Test
+testsString2Roll = TestList (map (uncurry testString2Roll) testDataString2Roll)
 
+testDataAddRoll :: [(Roll, Game, Game)]
+testDataAddRoll = [(roll 0,([],[]),([],[roll 0])),
+                   (roll 3,([],[roll 0]),([normal 0 3],[])),
+                   (roll 6,([normal 0 3],[roll 4]),([normal 0 3, spare 4],[])),
+                   (roll 9,([normal 0 3],[roll 1]),([normal 0 3, spare 1],[])),
+                   (roll 10,([normal 0 3],[roll 0]),([normal 0 3, spare 0],[])),
+                   (roll 10,([normal 0 3],[]),([normal 0 3, Strike],[])),
+                   (roll 11,([normal 0 3],[]),([normal 0 3],[])),
+                   (roll 6,([],[roll 5]),([],[roll 5]))]
 
-main :: IO Counts
-main = runTestTT tests_string2Roll
+testAddRoll :: Roll -> Game -> Game -> Test
+testAddRoll r g0 g1 = TestCase(assertEqual "Did not correctly add roll to game." g1 (addRoll g0 r))
+
+testsAddRoll :: Test
+testsAddRoll = TestList (map (uncurryN testAddRoll) testDataAddRoll)
+
+runTests :: IO Counts
+runTests = runTestTT.TestList $ [testsString2Roll,testsAddRoll]
+
+play :: Game -> IO Game
+play g0 = do
+    putStrLn "Enter Roll: "
+    r <- readRoll
+    let g1 = maybe g0 (addRoll g0) r
+    print $ showGame g1
+    play g1
